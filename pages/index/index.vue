e03a776958f323d1afcf7519c4214df20d2e704e
liuy <1319982103@qq.com>
2020-09-12 16:51:11 +0800

    插值表达式

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		hello uni-app{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'nihao'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
